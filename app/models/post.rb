76e85e7469e1b30355c008344973cb3dff8ec75c
Apprentice <apprentice@dbc-chicago-005.local>
2014-03-31 15:00:39 -0500

    Models created. Validations included

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :comments
+
+  validates :title, :url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: "creator_id"
   has_many :comments
 
-  validates :username, presence: true
-
+  validates :username, presence: true, uniqueness: true
+  validates :email, format: { with: /\w+[._$!]?\w+@\w+.\w{2,6}/,
+    message: "Only valid emails allowed" }
+  validates :password, length: { in: 4..10 }
 end
