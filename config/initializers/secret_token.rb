ba7258ed8f8d8ae70d9ea5c02ae70fbdce200b31
Apprentice <apprentice@dbc-chicago-005.local>
2014-03-31 14:03:58 -0500

    initial setup on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '80a66ebe46be823f3b27744d7250713bd938dcc60444c3295b14ca3d520f9ee7457aeaf96e00edea4c80d8b584b60a330cc5daa9dab5254a87a71c4fc992adf8'
